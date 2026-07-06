671e31a3a6cfd8685cefc8ae6106e269d63edd01
Wille Malmikumpu <wille.malmikumpu@hotmail.com>
2026-07-06 15:39:09 +0300

    1.5

--- part_1/courseinfo/src/App.jsx
+++ part_1/courseinfo/src/App.jsx
@@ -13,7 +13,6 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
-  console.log(props)
   return (
     <p>
       Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
@@ -22,35 +21,36 @@ const Total = (props) => {
 }
 
 const Part = (props) => {
-  console.log(props)
-
   return (
     <p>{props.part.name} {props.part.exercises}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
