59a3e81fa0046076691c69f776e0382d5c452a53
Wille Malmikumpu <wille.malmikumpu@hotmail.com>
2026-07-07 10:37:36 +0300

    2.6

---
+++ part_2/phonebook/src/App.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <table>
+        <tbody>
+          {persons.map(person => <tr key={person.name}><td>{person.name}</td></tr>)}
+        </tbody>
+      </table>
+
+    </div>
+  )
+
+}
+
+export default App
