33c5dd0eb3d38d192c8823fa834975826d3c70f1
Wille Malmikumpu <wille.malmikumpu@hotmail.com>
2026-07-17 21:13:58 +0300

    3.18

--- part_3/phonebook_backend/index.js
+++ part_3/phonebook_backend/index.js
@@ -30,19 +30,27 @@ app.get('/', (request, response) => {
 })
 
 app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people.</p> <p>${new Date(Date.now())}</p>`)
+  Person.countDocuments({}).then(count => {
+    response.send(`<p>Phonebook has info for ${count} people.</p> <p>${new Date(Date.now())}</p>`)
+  })
 })
 
 app.get('/api/persons', (request, response) => {
   Person.find({}).then(persons => {
     response.json(persons)
-  })
+  }).catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
